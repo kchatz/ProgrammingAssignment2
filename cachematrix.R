5fb1dbcd9ff18aa22512a10895e02be88dc1a1b4
kchatz <kchatz@outlook.com>
2015-04-26 16:28:28 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,36 +3,30 @@
 ## matrix inverse calculation; when the inverse is called the saved value is returned instead of
 ## repeating the calculation.
 
-## This function creates a special "matrix" object, which is really a list 
-## containing a function to
+## The function makeCacheMatrix creates a special "matrix" object; a list that contains a function to 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
 ## 3. set the value of the inverse
 ## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        ## create a matrix object x and some associated sub-functions/methods
+        ## create a matrix object x and the respective sub-functions/methods
         
         ## define the cache m
         m <- NULL
         set <- function(y) {
-                x <<- y ## assign the input matrix y to the variable x in the
-                ## parent environment
+                x <<- y ## assign the input matrix y to the variable x
                 m <<- NULL ## re-initialize m in the parent environment to null
         }
         get <- function() x ## return the matrix x
-        setinverse <- function(inverse) m <<- inverse ## set the cache m equal
-        ## to the inverse of the matrix x
+        setinverse <- function(inverse) m <<- inverse ## set the cache m equal to matrix x inverse
         getinverse <- function() m ## return the cached inverse of x
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## The following function calculates the inverse of the special "matrix" created
-## with the above function. However, it first checks to see if the inverse
-## has already been caclulated. If so, it 'get's the inverse from the cache
+## The function cacheSolve calculates the inverse of the special "matrix" created with makeCacheMatrix.
+## Before the calculation it checks if the inverse has already been caclulated. 
+## If so, it 'get's the inverse from the cache
 ## and skips the computation. Otherwise, it calculates the matrix inverse
 ## and sets the value of the inverse in the cache via the 'setinverse' function.
 
